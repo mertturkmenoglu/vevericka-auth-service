d4a2435d0e1d8b687a46e60c581ed2f1edd6488c
Mert <mertturkmenoglu99@gmail.com>
2020-10-11 18:37:24 +0300

    Remove alphanum condition from username validation

--- src/validation.js
+++ src/validation.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const registerSchema = Joi.object({
-	username: Joi.string().alphanum().max(32).required(),
+	username: Joi.string().max(32).required(),
 	name: Joi.string().min(6).max(255).required(),
 	email: Joi.string().min(6).max(255).email().required(),
 	password: Joi.string().min(8).max(1024).required()
